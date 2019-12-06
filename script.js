522306d495bf523840ac94a9a43021695e643e53
Shannon Zylstra <shannon.rae.zylstra@gmail.com>
2019-12-06 17:25:46 +0000

    Added a DOMContentLoaded listener.

--- script.js
+++ script.js
@@ -39,95 +39,103 @@ var sliceWordArray = (arr, n) => {
 	return arr
 }
 
-// get word string
-var wordString = getWordString().toLowerCase()
-// make wordString into array
-var words = wordString.split(',')
-// print words!
-console.log(words)
-// shuffle words
-shuffleWordArray(words)
-// print words again!
-console.log(words)
+const start = () => {
 
-// create 2D word array
-var boardArray = new Array(5)
-for (var i = 0; i < boardArray.length; i++) {
-	let j = 5*i
-	boardArray[i] = words.slice(j,j+5)
-}
-console.log(boardArray)
+	// get word string
+	var wordString = getWordString().toLowerCase()
+	// make wordString into array
+	var words = wordString.split(',')
+	// print words!
+	console.log(words)
+	// shuffle words
+	shuffleWordArray(words)
+	// print words again!
+	console.log(words)
 
-// create 2D color array
-const colors = []
-for (let i = 0; i < 9; i++) { // add 9 reds
-	colors.push('red')
-}
-for (let i = 0; i < 8; i++) { // add 8 blues
-	colors.push('blue')
-}
-for (let i = 0; i < 7; i++ ) { // add 7 whites
-	colors.push('neutral')
-}
-colors.push('assassin') // add 1 black
-shuffleWordArray(colors) // shuffle colors array
-var colorArray = new Array(5) // initialize new array
-for (var i = 0; i < colorArray.length; i++) { // loop over array
-	let j = 5*i
-	colorArray[i] = colors.slice(j,j+5) // add row to array
-}
-console.log(colorArray) // print to console
-
-// add colors to boardArray 
-var coloredBoardArray = new Array(5)
-for (let i = 0; i < 5; i++) {
-	coloredBoardArray[i] = []
-	for (let j = 0; j < 5; j++) {
-		coloredBoardArray[i][j] = [boardArray[i][j],colorArray[i][j]]
-		console.log(`(${i},${j}): [${boardArray[i][j]}, ${colorArray[i][j]}]`)
+	// create 2D word array
+	var boardArray = new Array(5)
+	for (var i = 0; i < boardArray.length; i++) {
+		let j = 5*i
+		boardArray[i] = words.slice(j,j+5)
 	}
-}
-console.log(coloredBoardArray)
+	console.log(boardArray)
 
-// make innerHTML
-var makeTableHtmlString = () => {
-	let tableHtmlString = `\n<div id="board" class="gridContainer">\n` // instead of <table id="boardTable">
-	let i = 0
-	boardArray.forEach(row => {
-		tableHtmlString += `\n<tr class=">\n`
+	// create 2D color array
+	const colors = []
+	for (let i = 0; i < 9; i++) { // add 9 reds
+		colors.push('red')
+	}
+	for (let i = 0; i < 8; i++) { // add 8 blues
+		colors.push('blue')
+	}
+	for (let i = 0; i < 7; i++ ) { // add 7 whites
+		colors.push('neutral')
+	}
+	colors.push('assassin') // add 1 black
+	shuffleWordArray(colors) // shuffle colors array
+	var colorArray = new Array(5) // initialize new array
+	for (var i = 0; i < colorArray.length; i++) { // loop over array
+		let j = 5*i
+		colorArray[i] = colors.slice(j,j+5) // add row to array
+	}
+	console.log(colorArray) // print to console
+
+	// add colors to boardArray 
+	var coloredBoardArray = new Array(5)
+	for (let i = 0; i < 5; i++) {
+		coloredBoardArray[i] = []
+		for (let j = 0; j < 5; j++) {
+			coloredBoardArray[i][j] = [boardArray[i][j],colorArray[i][j]]
+			console.log(`(${i},${j}): [${boardArray[i][j]}, ${colorArray[i][j]}]`)
+		}
+	}
+	console.log(coloredBoardArray)
+
+	// make innerHTML
+	var makeTableHtmlString = () => {
+		let tableHtmlString = `\n<div id="board" class="gridContainer">\n` // instead of <table id="boardTable">
+		let i = 0
+		boardArray.forEach(row => {
+			tableHtmlString += `\n<tr class=">\n`
+			row.forEach(word => {
+				tableHtmlString += `<td>${i+=1 }</td>`
+			})
+			tableHtmlString += `\n</tr>\n`
+		})
+		tableHtmlString += `\n</table>\n`
+		document.getElementById("board").innerHTML = tableHtmlString
+	}
+
+	let tableHtmlString = `\n<table>\n`
+	coloredBoardArray.forEach(row => {
+		tableHtmlString += `\n<tr>\n`
 		row.forEach(word => {
-			tableHtmlString += `<td>${i+=1 }</td>`
+			tableHtmlString += `<td class="${word[1]}">${word[0]}</td>`
 		})
 		tableHtmlString += `\n</tr>\n`
 	})
 	tableHtmlString += `\n</table>\n`
+	console.log(tableHtmlString)
 	document.getElementById("board").innerHTML = tableHtmlString
+
+	function chooseMode(mode) {
+		return mode
+	}
+
+	console.log(chooseMode("player"))
+
+	var changeMode = (modeToSet) => {
+		let modeElement = document.getElementsByClassName("mode")
+		console.log(modeElement)
+		let modeID = modeElement[0].id
+		console.log(`modeID = ${modeID}; modeToSet = ${modeToSet}`)
+		modeElement[0].id = modeToSet
+		// this works: document.getElementById(modeToSet).innerHTML = 'spy'
+	}
+	changeMode('spymaster')
 }
 
-let tableHtmlString = `\n<table>\n`
-coloredBoardArray.forEach(row => {
-	tableHtmlString += `\n<tr>\n`
-	row.forEach(word => {
-		tableHtmlString += `<td class="${word[1]}">${word[0]}</td>`
-	})
-	tableHtmlString += `\n</tr>\n`
+document.addEventListener('DOMContentLoaded', () => {
+	start()
+	// document.getElementById('start').addEventListener('click', start)
 })
-tableHtmlString += `\n</table>\n`
-console.log(tableHtmlString)
-document.getElementById("board").innerHTML = tableHtmlString
-
-function chooseMode(mode) {
-	return mode
-}
-
-console.log(chooseMode("player"))
-
-var changeMode = (modeToSet) => {
-	let modeElement = document.getElementsByClassName("mode")
-	console.log(modeElement)
-	let modeID = modeElement[0].id
-	console.log(`modeID = ${modeID}; modeToSet = ${modeToSet}`)
-	modeElement[0].id = modeToSet
-	// this works: document.getElementById(modeToSet).innerHTML = 'spy'
-}
-changeMode('spymaster')
